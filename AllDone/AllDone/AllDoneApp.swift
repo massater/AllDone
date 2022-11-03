9435b88ae031fdbfd9ebe1e67da1aa6b00327530
muttley <muttley@muttleys-iPro.attlocal.net>
2022-11-03 15:21:36 -0700

    Setup Project Done

---
+++ AllDone/AllDone/AllDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneApp.swift
+//  AllDone
+//
+//  Created by muttley on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
