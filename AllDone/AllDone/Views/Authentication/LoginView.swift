282afcd5b8455f91b2f3cfe3f2d7b45a8e184f3f
muttley <muttley@muttleys-iPro.attlocal.net>
2022-11-03 21:18:13 -0700

    Added Email TextField

--- AllDone/AllDone/Views/Authentication/LoginView.swift
+++ AllDone/AllDone/Views/Authentication/LoginView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
         ZStack {
             BackgroundGradientView()
             
             VStack {
                 LogoView()
-            }
-        }
+                    .padding(.bottom, 25)
+                
+                VStack(spacing: 20) {
+                    EmailTextField(text: $email)
+                        .padding(.horizontal, 32)
+                }  // End of VStack
+                
+            } // End of VStack
+            
+        } // End of ZStack
     }
 }
 
